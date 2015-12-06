99c9ac23f347c05ee3af3fe3ec78ef4d5cd9a708
mpmeguire <warfusion@outlook.com>
2015-12-06 17:00:28 -0500

    Update bankserver.h

--- bankserver.h
+++ bankserver.h
@@ -5,6 +5,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <pthread.h>
 
 
 typedef struct
@@ -16,13 +17,25 @@ typedef struct
 
 } account;
 
+typedef struct
+{
+
+	pthread_t *incoming;
+	pthread_t *outgoing;
+	int socket_FD;
+	pthread_t *next;
+
+} thread_node;
 
 account* start_account_session(char*, account*[]);
 account* finish_account_session(account*);
 account* credit_account(account*, float);
 account* debit_account(account*, float);
 int* add_socket_FD(int, int[]);
+void start_threads(thread_node*, int);
 int* remove_socket_FD(int, int[]);
 void open_account(char*, account*[]);
 void print_account_list(account *account_list[]);
 void start_server();
+void read_input(int);
+void write_output(int);
