3d06453cb94a9aa127ad8fae6a0e192b893b9c24
mpmeguire <warfusion@outlook.com>
2015-12-06 10:13:20 -0500

    Update bank.c

--- bank.c
+++ bank.c
@@ -5,22 +5,22 @@
 #include<unistd.h>
 
 pthread_t tid[2];
+int counter;
+pthread_mutex_t lock;
 
 void* doSomeThing(void *arg)
 {
-    unsigned long i = 0;
-    pthread_t id = pthread_self();
+    pthread_mutex_lock(&lock);
 
-    if(pthread_equal(id,tid[0]))
-    {
-        printf("\n First thread processing\n");
-    }
-    else
-    {
-        printf("\n Second thread processing\n");
-    }
+    unsigned long i = 0;
+    counter += 1;
+    printf("\n Job %d started\n", counter);
 
     for(i=0; i<(0xFFFFFFFF);i++);
+
+    printf("\n Job %d finished\n", counter);
+
+    pthread_mutex_unlock(&lock);
 
     return NULL;
 }
@@ -30,17 +30,23 @@ int main(void)
     int i = 0;
     int err;
 
+    if (pthread_mutex_init(&lock, NULL) != 0)
+    {
+        printf("\n mutex init failed\n");
+        return 1;
+    }
+
     while(i < 2)
     {
         err = pthread_create(&(tid[i]), NULL, &doSomeThing, NULL);
         if (err != 0)
             printf("\ncan't create thread :[%s]", strerror(err));
-        else
-            printf("\n Thread created successfully\n");
-
         i++;
     }
 
-    sleep(5);
+    pthread_join(tid[0], NULL);
+    pthread_join(tid[1], NULL);
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
