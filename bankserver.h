c01829891692dd7d370e46fbde30cd04ba642357
mpmeguire <warfusion@outlook.com>
2015-12-06 21:17:23 -0500

    Update bankserver.h

--- bankserver.h
+++ bankserver.h
@@ -20,7 +20,7 @@ typedef struct
 struct thread_node_t
 {
 
-	pthread_t *thread;
+	pthread_t thread;
 	int socket_FD;
 	struct thread_node_t *next;
 
@@ -28,15 +28,18 @@ struct thread_node_t
 
 typedef struct thread_node_t thread_node;
 
+void disconnect_client(int);
+pthread_mutex_t lock;
 account *list[20];
-account* start_account_session(char*, account*[]);
+account* start_account_session(char*);
 account* finish_account_session(account*);
 account* credit_account(account*, float);
 account* debit_account(account*, float);
-void add_thread(thread_node*, int);
+void add_thread(thread_node**, int);
+char find_command(char*);
 void start_threads(thread_node*, int);
 int* remove_socket_FD(int, int[]);
-void open_account(char*, account*[]);
+void open_account(char*);
 void print_account_list(account *account_list[]);
 void start_server();
 void client_service_thread(int);
